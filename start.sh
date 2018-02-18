9d9451965e5b83716886beea3bb7570c62d17378
Ashish Thakwani <athakwani@gmail.com>
2018-02-17 22:11:47 -0800

    Route external traffic to localhost:80

--- start.sh
+++ start.sh
@@ -4,4 +4,4 @@ ifconfig
 
 IP="$(ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1' | grep 172)"
 
-socat TCP4-LISTEN:80,fork,bind=$IP,reuseaddr TCP:127.0.0.1:8080
+socat TCP4-LISTEN:80,fork,bind=$IP,reuseaddr TCP:127.0.0.1:80
